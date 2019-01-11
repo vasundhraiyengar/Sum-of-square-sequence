16b54bc2222bf0e527e8730a493ec57d6cc1e08f
Vasundhra Iyengar <vasundhra.iyengar@gmail.com>
2019-01-10 19:44:16 -0500

    Update proj1.ex

--- lib/proj1.ex
+++ lib/proj1.ex
@@ -1,41 +1,38 @@
 defmodule Project1 do
+  use GenServer
 
-    use GenServer
+  def main(args) do
+    n = String.to_integer(hd(args))
+    k = String.to_integer(List.last(args))
 
-    def main(args) do
+    Enum.each(1..n, fn num ->
+      # spawns n processes from 1 to n
+      {:ok, pid} = Project1.start_link()
+      Project1.perform_calcs(pid, num, k)
+    end)
+  end
 
-       n=String.to_integer(hd(args))
-       k=String.to_integer(List.last(args))
+  def start_link do
+    GenServer.start_link(__MODULE__, [])
+  end
 
-       Enum.each(1..n, fn(num)->
-         {:ok,pid} = Project1.start_link               #spawns n processes from 1 to n
-         Project1.perform_calcs(pid,num,k)
-       end)
+  def init([]) do
+    {:ok, []}
+  end
 
-    end
+  def perform_calcs(process_id, n1, k1) do
+    # Handle cast call for processing sum and root
+    GenServer.cast(process_id, {:async_sum, n1, k1})
+  end
 
-    def start_link do
-      GenServer.start_link(__MODULE__,[])
-    end
+  def handle_cast({:async_sum, n, k}, state) do
+    last = n + k - 1
+    # gives the square root of the sum of sequence
+    var_sqrt = Enum.map(n..last, fn x -> x * x end) |> Enum.sum() |> :math.sqrt()
+    var_floor = Float.floor(var_sqrt)
 
-    def init([]) do
-      {:ok, []}
-    end
-
-    def perform_calcs(process_id, n1, k1) do
-      GenServer.cast(process_id, {:async_sum, n1, k1})              #Handle cast call for processing sum and root
-    end
-
-    def handle_cast({:async_sum, n, k}, state) do
-
-      last=n+k-1
-      var_sqrt=Enum.map(n..last, fn x-> x*x end)|>Enum.sum|>:math.sqrt      #gives the square root of the sum of sequence
-      var_floor=Float.floor(var_sqrt)
-
-      if ((var_sqrt - var_floor) == 0),do:                                  #checks for perfect square
-        IO.puts("#{n}")
-        {:stop, :normal, state}
-
-    end
-
+    # checks for perfect square
+    if var_sqrt - var_floor == 0, do: IO.puts("#{n}")
+    {:stop, :normal, state}
+  end
 end
